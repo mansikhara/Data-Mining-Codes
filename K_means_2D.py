0fea16eee041dbc87ee7d3ae92c161cfc2423921
Mansi Khara <kharamansi11@gmail.com>
2017-07-16 09:11:34 -0700

    Update K_means_2D.py

--- K_means_2D.py
+++ K_means_2D.py
@@ -23,13 +23,21 @@ df['y'] = y
 # renaming cols
 df.columns = ['x', 'y']
 
+#random values for mean
 random_index = np.random.randint(0, len(df))
-random_index1=np.random.randint(0,len(df))
 print(random_index)
+random_index1=np.random.randint(0,len(df))
+if random_index == random_index1:
+    while random_index!=random_index1:
+        random_index1 = np.random.randint(0, len(df))
 print(random_index1)
+
+
+
 random_x, random_y = df.iloc[random_index]
 rand_x,rand_y=df.iloc[random_index1]
-
+print(random_x,random_y)
+print(rand_x,rand_y)
 df['ed'] = df[['x','y']].apply(eucledian_distance,axis=1)
 df['ed'].head()
 random_x=rand_x
@@ -42,73 +50,30 @@ cluster_1 = pd.DataFrame(df[df['ed'] > df['ed1']][['x', 'y']])
 cluster_2 = pd.DataFrame(df[df['ed'] < df['ed1']][['x', 'y']])
 
 print("1 Iteration")
-print("cluster1:")
+print("Cluster 1:")
 print(cluster_1)
-print("cluster2:")
+print("Cluster 2:")
 print(cluster_2)
 
-mean1_x=0
-for i in range(0,len(cluster_1.index)):
-    mean1_x=mean1_x+(cluster_1['x'].iloc[i])
-mean1_x=mean1_x/len(cluster_1.index)
-print(mean1_x)
 
-mean1_y=0
-for i in range(0,len(cluster_1.index)):
-    mean1_y=mean1_y+(cluster_1['y'].iloc[i])
-mean1_y=mean1_y/len(cluster_1.index)
-print(mean1_y)
+cluster_1new=cluster_1
+cluster_2new=cluster_2
+cluster_2=pd.DataFrame()
+cluster_1=pd.DataFrame()
+counter = 2
+while not cluster_1new.equals(cluster_1):
 
-print("Mean 1:",mean1_x,mean1_y)
-
-mean2_x=0
-for i in range(0,len(cluster_2.index)):
-    mean2_x=mean2_x+(cluster_2['x'].iloc[i])
-mean2_x=mean2_x/len(cluster_2.index)
-print(mean2_x)
-
-mean2_y=0
-for i in range(0,len(cluster_2.index)):
-    mean2_y=mean2_y+(cluster_2['y'].iloc[i])
-mean2_y=mean2_y/len(cluster_2.index)
-print(mean2_y)
-
-print("Mean 2:",mean2_x,mean2_y)
-
-random_x=mean1_x
-random_y=mean1_y
-df['ed'] = df[['x','y']].apply(eucledian_distance,axis=1)
-df['ed'].head()
-random_x=mean2_x
-random_y=mean2_y
-df['ed1'] = df[['x','y']].apply(eucledian_distance,axis=1)
-df['ed1'].head()
-print(df)
-
-cluster_1new = pd.DataFrame(df[df['ed'] > df['ed1']][['x', 'y']])
-cluster_2new = pd.DataFrame(df[df['ed'] < df['ed1']][['x', 'y']])
-
-print("2 Iteration")
-print("cluster1:")
-print(cluster_1new)
-print("cluster2:")
-print(cluster_2new)
-
-cluster_1.loc['x', 'y'] = cluster_1new[['x', 'y']]
-print(cluster_1)
-'''
-while cluster_1new != cluster_1:
     mean1_x = 0
     for i in range(0, len(cluster_1new.index)):
         mean1_x = mean1_x + (cluster_1new['x'].iloc[i])
     mean1_x = mean1_x / len(cluster_1new.index)
-    print(mean1_x)
+
 
     mean1_y = 0
     for i in range(0, len(cluster_1new.index)):
         mean1_y = mean1_y + (cluster_1new['y'].iloc[i])
     mean1_y = mean1_y / len(cluster_1new.index)
-    print(mean1_y)
+
 
     print("Mean 1:", mean1_x, mean1_y)
 
@@ -116,19 +81,31 @@ while cluster_1new != cluster_1:
     for i in range(0, len(cluster_2new.index)):
         mean2_x = mean2_x + (cluster_2new['x'].iloc[i])
     mean2_x = mean2_x / len(cluster_2new.index)
-    print(mean2_x)
+
 
     mean2_y = 0
     for i in range(0, len(cluster_2new.index)):
         mean2_y = mean2_y + (cluster_2new['y'].iloc[i])
     mean2_y = mean2_y / len(cluster_2new.index)
-    print(mean2_y)
+
 
     print("Mean 2:", mean2_x, mean2_y)
 
-    
-    print(cluster_1)
-'''
+    del cluster_1
+    cluster_1 = cluster_1new
+
+    del cluster_2
+    cluster_2 = cluster_2new
+
+    cluster_1new = pd.DataFrame(df[df['ed'] > df['ed1']][['x', 'y']])
+    cluster_2new = pd.DataFrame(df[df['ed'] < df['ed1']][['x', 'y']])
+    print(counter,"Iteration")
+    counter=counter+1
+    print("cluster1:")
+    print(cluster_1new)
+    print("cluster2:")
+    print(cluster_2new)
+
 
 
 
